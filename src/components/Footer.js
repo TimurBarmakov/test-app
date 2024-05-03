a396e26f364003d38b4b36cdb93894f728624f66
Timur Barmakov <tbarmakov@bk.ru>
2024-05-03 14:23:08 +0500

    fifth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,41 +5,53 @@ import SecondFooterImage from '../assets/images/image-gaming-growth.jpg';
 import ThirdFooterImage from '../assets/images/image-top-laptops.jpg';
 
 const Footer = () => {
+    const footerData = [
+        {
+            id: 1,
+            imgSrc: FirstFooterImage,
+            title: "01",
+            subTitle: "Reviving Retro PCs",
+            description: "What happens when old PCs are given modern upgrades?"
+        },
+        {
+            id: 2,
+            imgSrc: SecondFooterImage,
+            title: "02",
+            subTitle: "Top 10 Laptops of 2022",
+            description: "Our best picks for various needs and budgets."
+        },
+        {
+            id: 3,
+            imgSrc: ThirdFooterImage,
+            title: "03",
+            subTitle: "The Growth of Gaming",
+            description: "How the pandemic has sparked fresh opportunities."
+        },
+        {
+            id: 4,
+            imgSrc: ThirdFooterImage,
+            title: "04",
+            subTitle: "The Growth of Gaming",
+            description: "How the pandemic has sparked fresh opportunities."
+        }
+    ];
+
     return (
         <footer>
             <div className="container">
-                <ul className="footerList">
-                    <li>
-                        <div className="footerImageContainer">
-                            <img src={FirstFooterImage} alt="Основное изображение" />
-                        </div>
-                        <div className="footerDescription">
-                            <h2>01</h2>
-                            <h3>Reviving Retro PCs</h3>
-                            <p>What happens when old PCs are given modern upgrades?
-                            </p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="footerImageContainer">
-                            <img src={SecondFooterImage} alt="Основное изображение" />
-                        </div>
-                        <div className="footerDescription">
-                            <h2>02</h2>
-                            <h3>Top 10 Laptops of 2022</h3>
-                            <p>Our best picks for various needs and budgets.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="footerImageContainer">
-                            <img src={ThirdFooterImage} alt="Основное изображение" />
-                        </div>
-                        <div className="footerDescription">
-                            <h2>03</h2>
-                            <h3>The Growth of Gaming</h3>
-                            <p>How the pandemic has sparked fresh opportunities.</p>
-                        </div>
-                    </li>
+                    <ul className="footerList">
+                    {footerData.map((item) => (
+                        <li key={item.id}>
+                            <div className="footerImageContainer">
+                                <img src={item.imgSrc} alt="Основное изображение" />
+                            </div>
+                            <div className="footerDescription">
+                                <h2>{item.title}</h2>
+                                <h3>{item.subTitle}</h3>
+                                <p>{item.description}</p>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </footer>
